cbe0b7f59582c4aecd4848b0a2e2e23cb47e2e8a
Camila <ucami98@gmail.com>
2024-09-09 19:09:17 +1000

    Se agrega codigo a app.py, 2 variables que guardan los resultados del cointoss y lo muestran en una tabla

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+import pandas as pd
 import scipy.stats
 import streamlit as st
 import time
+
+# estas son variables de estado que se conservan cuando Streamlin vuelve a ejecutar este script
+if 'experiment_no' not in st.session_state:
+    st.session_state['experiment_no'] = 0
+
+if 'df_experiment_results' not in st.session_state:
+    st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iteraciones', 'media'])
+
 
 st.header('Lanzar una moneda')
 
@@ -27,6 +36,20 @@ def toss_coin(n): # función que emula el lanzamiento de una moneda
 number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
 start_button = st.button('Ejecutar')
 
+
 if start_button:
     st.write(f'Experimento con {number_of_trials} intentos en curso.')
+    st.session_state['experiment_no'] += 1
     mean = toss_coin(number_of_trials)
+    st.session_state['df_experiment_results'] = pd.concat([
+        st.session_state['df_experiment_results'],
+        pd.DataFrame(data=[[st.session_state['experiment_no'],
+                            number_of_trials,
+                            mean]],
+                     columns=['no', 'iterations', 'mean'])
+        ],
+        axis=0)
+    st.session_state['df_experiment_results'] = 
+        st.session_state['df_experiment_results'].reset_index(drop=True)
+
+st.write(st.session_state['df_experiment_results'])
